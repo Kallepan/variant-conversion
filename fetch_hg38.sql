5f43150d81d27cc61fdaa49ac10a960c1dd589d1
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2024-02-13 10:29:15 +0000

    feat: added query to fetch

---
+++ fetch_hg38.sql
@@ -0,0 +1,27 @@
+SELECT *
+FROM variants_db_variant
+WHERE
+    (genomic_reference = 'NC_000001' AND accession_number = '11') OR
+    (genomic_reference = 'NC_000002' AND accession_number ='12') OR
+    (genomic_reference = 'NC_000003' AND accession_number ='12') OR
+    (genomic_reference = 'NC_000004' AND accession_number ='12') OR
+    (genomic_reference = 'NC_000005' AND accession_number ='10') OR
+    (genomic_reference = 'NC_000006' AND accession_number ='12') OR
+    (genomic_reference = 'NC_000007' AND accession_number ='14') OR
+    (genomic_reference = 'NC_000008' AND accession_number ='11') OR
+    (genomic_reference = 'NC_000009' AND accession_number ='12') OR
+    (genomic_reference = 'NC_000010' AND accession_number ='11') OR
+    (genomic_reference = 'NC_000011' AND accession_number ='10') OR
+    (genomic_reference = 'NC_000012' AND accession_number ='12') OR
+    (genomic_reference = 'NC_000013' AND accession_number ='11') OR
+    (genomic_reference = 'NC_000014' AND accession_number ='9') OR
+    (genomic_reference = 'NC_000015' AND accession_number ='10') OR
+    (genomic_reference = 'NC_000016' AND accession_number ='10') OR
+    (genomic_reference = 'NC_000017' AND accession_number ='11') OR
+    (genomic_reference = 'NC_000018' AND accession_number ='10') OR
+    (genomic_reference = 'NC_000019' AND accession_number ='10') OR
+    (genomic_reference = 'NC_000020' AND accession_number ='11') OR
+    (genomic_reference = 'NC_000021' AND accession_number ='9') OR
+    (genomic_reference = 'NC_000022' AND accession_number ='11') OR
+    (genomic_reference = 'NC_000023' AND accession_number ='11') OR
+    (genomic_reference = 'NC_000024' AND accession_number ='10')
