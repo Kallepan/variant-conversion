93f5120fe881b4cc53809de5128f5d1ff121c6f8
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2024-02-22 07:14:39 +0000

    feat: inserted data into table

--- rms-migration/db_import.py
+++ rms-migration/db_import.py
@@ -1,40 +1,24 @@
 """
 Script to import multiple antibodies into the database from a csv file
 """
-import psycopg
+
 import os
 import csv
 import logging
 import sys
+import uuid
 
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
 DB_USER = os.environ.get("DB_USER")
 DB_NAME = os.environ.get("DB_NAME")
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
-CSV_FILE = os.path.join(os.path.dirname(__file__), "res","types.csv")
+CSV_FILE = os.path.join(os.path.dirname(__file__), "res", "types.csv")
+OUTPUT_FILE = os.path.join(os.path.dirname(__file__), "queries.sql")
 
-TABLE_NAME = # TODO CHANGEME
-
+TABLE_NAME = "bak.type"
 logging.basicConfig(level=logging.DEBUG)
 
-def connect():
-    logging.info(f"Connecting to {DB_HOST}:{DB_PORT} as {DB_USER} on {DB_NAME} with password {DB_PASSWORD}")
-
-    conn = psycopg.connect(
-        host=DB_HOST, user=DB_USER, password=DB_PASSWORD, dbname=DB_NAME, port=DB_PORT
-    )
-    return conn
-
-def test_connection(conn):
-    cursor = conn.cursor()
-    cursor.execute("SELECT 1")
-    result = cursor.fetchone()
-    cursor.close()
-    assert result[0] == 1
-    logging.info("Connection successful")
-
-    sys.exit(0)
 
 def read_csv():
     data = []
@@ -43,45 +27,41 @@ def read_csv():
         for row in reader:
             data.append(row)
 
+    data.pop(0)  # Remove header
+
     return data
 
-def insert_antibody(conn, row):
+
+def insert_antibody(row):
     name = f"{row[0]} ({row[1]})"
     producer = row[2]
     article_number = row[3]
+    id = str(uuid.uuid4())
 
-    cursor = conn.cursor()
-    query = f"INSERT INTO {TABLE_NAME} (name, producer, article_number) VALUES ('{name}', '{producer}', '{article_number}')"
-    try:
-        cursor.execute(
-            query
-        )
-        conn.commit()
-    except Exception as e:
-        conn.rollback()
-        raise e
-    finally:
-        cursor.close()
+    return f"INSERT INTO {TABLE_NAME} (id, name, producer, article_number, created_at) VALUES ('{id}', '{name}', '{producer}', '{article_number}', NOW());"
 
 
 def main():
+    if os.path.exists(OUTPUT_FILE):
+        logging.info(f"Removing old file {OUTPUT_FILE}")
+        os.remove(OUTPUT_FILE)
+
     data = read_csv()
-    conn = connect()
 
     # Debugging
     # test_connection(conn)
 
+    queries = []
     for i, row in enumerate(data):
         logging.debug(f"Updating row {i+1}/{len(data)}")
 
-        try:
-            insert_antibody(conn, row)
-        except Exception as e:
-            logging.error(f"Failed to insert antibody {row[0]}")
-            logging.error(e)
-            continue
-    
-    conn.close()
+        query = insert_antibody(row)
+
+        queries.append(query)
+
+    with open(OUTPUT_FILE, "w") as file:
+        file.write("\n".join(queries))
+
 
 if __name__ == "__main__":
     main()
